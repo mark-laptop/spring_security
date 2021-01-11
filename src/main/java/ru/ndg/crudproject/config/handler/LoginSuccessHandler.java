6c9149f584e86c5b30961c3152d86fb76e58581e
pro <ndg23@bk.ru>
2021-01-11 20:10:06 +0300

    add project

--- src/main/java/ru/ndg/crudproject/config/handler/LoginSuccessHandler.java
+++ src/main/java/ru/ndg/crudproject/config/handler/LoginSuccessHandler.java
@@ -1,7 +1,7 @@
 package ru.ndg.crudproject.config.handler;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 @Component(value = "simpleLoginSuccessHandler")
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -17,9 +18,10 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException, ServletException {
-        if (authentication.isAuthenticated() && authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (roles.contains("ROLE_ADMIN")) {
             httpServletResponse.sendRedirect("/admin");
-        } else if (authentication.isAuthenticated() && authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER"))){
+        } else if (roles.contains("ROLE_USER")) {
             httpServletResponse.sendRedirect("/user");
         } else {
             httpServletResponse.sendRedirect("/login");
